3a344f646713f2abec1575e66b52b1c2d07524dd
rodrigokang <rodrigokang88@gmail.com>
2024-09-16 23:15:13 -0300

    Improvements

--- retail_dashboard/.ipynb_checkpoints/dashboard-checkpoint.py
+++ retail_dashboard/.ipynb_checkpoints/dashboard-checkpoint.py
@@ -1,8 +1,12 @@
 # Import libraries
+
+import os
+os.system('pip install seaborn')
+import seaborn as sns
+
 import streamlit as st
 import requests
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from pathlib import Path
 
