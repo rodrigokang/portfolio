85c8529498c5388bdb7c81ab79bb2d775fa3ae16
rodrigokang <rodrigokang88@gmail.com>
2024-09-17 00:06:43 -0300

    Improvements

--- retail_dashboard/.ipynb_checkpoints/dashboard-checkpoint.py
+++ retail_dashboard/.ipynb_checkpoints/dashboard-checkpoint.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 # Define the base URL of Flask application
-base_url = 'http://127.0.0.1:5000'
+base_url = 'https://rfmbasedcustomersegmentationmodel.streamlit.app/'
 
 def authenticate_user(username, password):
     """
